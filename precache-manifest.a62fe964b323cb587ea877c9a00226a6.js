ebf2479d1270b2f0ac0e4c324a26461796e0b0d6
WooLuck <5dntjr5@naver.com>
2019-06-12 11:05:26 +0900

    Updates

---
+++ precache-manifest.a62fe964b323cb587ea877c9a00226a6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb282e790c345576c91094514b14913c",
+    "url": "/ProjectLuck/index.html"
+  },
+  {
+    "revision": "8001153c2dca508c207e",
+    "url": "/ProjectLuck/static/css/main.7020d46b.chunk.css"
+  },
+  {
+    "revision": "b0c5012ca7746f497091",
+    "url": "/ProjectLuck/static/js/2.ab1b0ea4.chunk.js"
+  },
+  {
+    "revision": "8001153c2dca508c207e",
+    "url": "/ProjectLuck/static/js/main.fc449bfb.chunk.js"
+  },
+  {
+    "revision": "be15c802bd3c8033268f",
+    "url": "/ProjectLuck/static/js/runtime~main.5137fffc.js"
+  }
+]);
